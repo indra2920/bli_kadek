ecaa4d5e2cd8ca4e293e1a3e3cc2dcfdd17c7ff2
warungonline03 <owarung04@gmail.com>
2026-05-09 17:13:46 +0700

    unify firebase database connection for owner and customer

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCnqXvxTKNqJP2IplZzw-Hrwreobsxn_aU",
+  authDomain: "bli-kadek-resto.firebaseapp.com",
+  projectId: "bli-kadek-resto",
+  storageBucket: "bli-kadek-resto.firebasestorage.app",
+  messagingSenderId: "661900320249",
+  appId: "1:661900320249:web:4d913fcd17b0065e6db128",
+  measurementId: "G-YQ4VPKDLE3"
 };
 
 const app = initializeApp(firebaseConfig);
